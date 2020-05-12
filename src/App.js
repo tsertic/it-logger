b52d5c6c5229b703d84400c350459c59b2fe0e58
Tomislav <tsertic5@gmail.com>
2020-05-12 15:40:16 +0100

    added prop-types, created LogItem component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import React, { useEffect } from "react";
+import React, { useEffect, Fragment } from "react";
 
 import "materialize-css/dist/css/materialize.min.css";
 import M from "materialize-css/dist/js/materialize.min.js";
 import "./App.css";
+import SearchBar from "./components/layout/SearchBar";
+import Logs from "./components/logs/Logs";
 
 const App = () => {
   useEffect(() => {
     //init materialize js
     M.AutoInit();
   });
-  return <div className="App">My app</div>;
+  return (
+    <Fragment>
+      <SearchBar />
+      <div className="container">
+        <Logs />
+      </div>
+    </Fragment>
+  );
 };
 
 export default App;
---
+++ src/components/logs/Logs.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+import LogItem from "./LogItem";
+
+const Logs = () => {
+  const [logs, setLogs] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    getLogs();
+    //esling-disable-next-line
+  }, []);
+
+  const getLogs = async () => {
+    setLoading(true);
+    const res = await fetch("/logs");
+    const data = await res.json();
+
+    setLogs(data);
+    setLoading(false);
+  };
+
+  if (loading) {
+    return <h4>Loading...</h4>;
+  }
+
+  return (
+    <ul className="collection with-header">
+      <li className="collection-header">
+        <h4 className="center">System Logs</h4>
+      </li>
+      {!loading && logs.length === 0 ? (
+        <p className="center">No Logs to show...</p>
+      ) : (
+        logs.map((log) => {
+          return <LogItem log={log} />;
+        })
+      )}
+    </ul>
+  );
+};
+
+export default Logs;
