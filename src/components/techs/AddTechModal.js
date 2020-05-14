821e4c77cdfde43ddc2505c1789600b1d51765ef
Tomislav <tsertic5@gmail.com>
2020-05-14 13:38:16 +0200

    modified addbtn component

---
+++ src/components/techs/AddTechModal.js
@@ -0,0 +1,85 @@
+import React, { useState } from "react";
+import M from "materialize-css/dist/js/materialize.min.js";
+
+const AddTechModal = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+
+  const onSubmit = () => {
+    if (firstName === "" || lastName === "") {
+      M.toast({ html: "please enter the first and last name" });
+    } else {
+      console.log(firstName, lastName);
+      //clear fields
+      setFirstName("");
+      setLastName("");
+    }
+  };
+
+  return (
+    <div id="add-tech-modal" className="modal">
+      <div className="modal-content">
+        <h4>New Technician</h4>
+        <div className="row">
+          <div className="input-field">
+            <input
+              type="text"
+              name="firstName"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+            />
+            <label htmlFor="message" className="active">
+              {" "}
+              Log Message
+            </label>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="input-field">
+            <select
+              name="tech"
+              value={tech}
+              className="browser-default"
+              onChange={(e) => setTech(e.target.value)}
+            >
+              <option value="" disabled>
+                Select Technican
+              </option>
+              <option value="John DOe">John DOe</option>
+              <option value="Sam Smith">Sam Smith</option>
+              <option value="Ines Basic">Ines Basic</option>
+            </select>
+          </div>
+        </div>
+        <div className="row">
+          <div className="input-field">
+            <p>
+              <label>
+                <input
+                  type="checkbox"
+                  className="filled-in"
+                  checked={attention}
+                  value={attention}
+                  onChange={(e) => setAttention(!attention)}
+                />
+                <span>Needs Attention</span>
+              </label>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="modal-footer">
+        <a
+          href="#!"
+          onClick={onSubmit}
+          class="modal-close waves-effect waves-green btn-flat"
+        >
+          Enter
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default AddTechModal;
