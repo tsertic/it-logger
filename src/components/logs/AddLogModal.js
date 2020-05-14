90c1a1c46469d8859ee760b5d7e6d50120b02099
Tomislav <tsertic5@gmail.com>
2020-05-14 12:02:00 +0200

    finished AddLogModal component

--- src/components/logs/AddLogModal.js
+++ src/components/logs/AddLogModal.js
@@ -1,9 +1,19 @@
 import React, { useState } from "react";
+import M from "materialize-css/dist/js/materialize.min.js";
 
 const AddLogModal = () => {
   const [message, setMessage] = useState("");
   const [attention, setAttention] = useState(false);
   const [tech, setTech] = useState("");
+
+  const onSubmit = () => {
+    if (message === "" || tech === "") {
+      M.toast({ html: "please enter a message and tech" });
+    } else {
+      console.log(message, tech, attention);
+    }
+  };
+
   return (
     <div id="add-log-modal" className="modal" style={modalStyle}>
       <div className="modal-content">
@@ -22,6 +32,49 @@ const AddLogModal = () => {
             </label>
           </div>
         </div>
+
+        <div className="row">
+          <div className="input-field">
+            <select
+              name="tech"
+              value={tech}
+              className="browser-default"
+              onChange={(e) => setTech(e.target.value)}
+            >
+              <option value="" disabled>
+                Select Technican
+              </option>
+              <option value="John DOe">John DOe</option>
+              <option value="Sam Smith">Sam Smith</option>
+              <option value="Ines Basic">Ines Basic</option>
+            </select>
+          </div>
+        </div>
+        <div className="row">
+          <div className="input-field">
+            <p>
+              <label>
+                <input
+                  type="checkbox"
+                  className="filled-in"
+                  checked={attention}
+                  value={attention}
+                  onChange={(e) => setAttention(!attention)}
+                />
+                <span>Needs Attention</span>
+              </label>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="modal-footer">
+        <a
+          href="#!"
+          onClick={onSubmit}
+          class="modal-close waves-effect waves-green btn-flat"
+        >
+          Enter
+        </a>
       </div>
     </div>
   );
