35d52e5ea59baab0c5ad7359081f107c24de904f
Tomislav <tsertic5@gmail.com>
2020-05-19 11:22:23 +0200

    added logs reducer,action and types

--- src/reducers/logReducer.js
+++ src/reducers/logReducer.js
@@ -1,3 +1,5 @@
+import { SET_LOADING, LOGS_ERROR, GET_LOGS } from "./../actions/types";
+
 const initialState = {
   logs: null,
   current: null,
@@ -7,6 +9,22 @@ const initialState = {
 
 export default (state = initialState, action) => {
   switch (action.type) {
+    case SET_LOADING:
+      return {
+        ...state,
+        loading: true,
+      };
+    case GET_LOGS:
+      return {
+        ...state,
+        loading: false,
+        logs: action.payload,
+      };
+    case LOGS_ERROR:
+      return {
+        ...state,
+        error: action.payload,
+      };
     default:
       return state;
   }
