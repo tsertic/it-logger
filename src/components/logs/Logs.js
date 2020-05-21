b669c3eae28a61ea350ade5e04314d272cb9c9fc
Tomislav <tsertic5@gmail.com>
2020-05-21 13:24:07 +0200

    added redux to Logs component

--- src/components/logs/Logs.js
+++ src/components/logs/Logs.js
@@ -2,26 +2,15 @@ import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
 import LogItem from "./LogItem";
 import Preloader from "../layout/Preloader";
-
-const Logs = () => {
-  const [logs, setLogs] = useState([]);
-  const [loading, setLoading] = useState(false);
-
+import PropTypes from "prop-types";
+import { getLogs } from "./../../actions/logActions";
+const Logs = ({ log: { logs, loading }, getLogs }) => {
   useEffect(() => {
     getLogs();
     //esling-disable-next-line
   }, []);
 
-  const getLogs = async () => {
-    setLoading(true);
-    const res = await fetch("/logs");
-    const data = await res.json();
-
-    setLogs(data);
-    setLoading(false);
-  };
-
-  if (loading) {
+  if (loading || logs === null) {
     return <Preloader />;
   }
 
@@ -41,8 +30,13 @@ const Logs = () => {
   );
 };
 
+Logs.propTypes = {
+  log: PropTypes.object.isRequired,
+  loading: PropTypes.bool.isRequired,
+};
+
 const mapStateToProps = (state) => ({
   log: state.log,
 });
 
-export default connect(mapStateToProps)(Logs);
+export default connect(mapStateToProps, { getLogs })(Logs);
